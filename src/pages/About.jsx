f73146ee5c1399001a454f3d7db777f383cca189
JoJobani <hvu2309@gmail.com>
2024-07-09 16:13:45 +0300

    Create file structure

---
+++ src/pages/About.jsx
@@ -0,0 +1,8 @@
+export function About(){
+    return (
+        <section>
+            <h2>About Us</h2>
+            <p>This site was made by Jonathan Menashe for Coding Academy</p>
+        </section>
+    )
+}
---
+++ src/pages/ToyDetails.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react"
+import { Link, Navigate, useParams } from "react-router-dom"
+
+// import {toyService} from "../services/toy.service.js"
+import {toyService} from "../services/toy.front-service.js"
+
+export function ToyDetails(){
+    const [toy,setToy] = useState(null)
+    const {toyId} = useParams()
+
+    useEffect(()=> {
+        if (toyId) loadToy()
+    },[toyId])
+
+    function loadToy(){
+        toyService.getById(toyId)
+            .then (toy => setToy(toy))
+            .catch (err => {
+                console.log('Had issues getting toy details ', err)
+                Navigate('/toy')
+            })
+    }
+
+    if (!toy) return <div>Loading...</div>
+    return (
+        <section className="toy-details">
+            <h1>{toy.name}</h1>
+            <h5>Price: {toy.price}</h5>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo laboriosam sunt vero, vitae ducimus ullam quis explicabo neque quam voluptate numquam quibusdam repellendus veniam saepe? Molestias, assumenda! Similique, praesentium facilis?</p>
+            <Link to={`/toy/edit/${toy._id}`}>Edit</Link>
+            <Link to={`/toy`}>Back</Link>
+        </section>
+    )
+
+}
