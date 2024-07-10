461fa7a1cf15fbf09172c099a6fa5deec80773b8
JoJobani <hvu2309@gmail.com>
2024-07-10 19:00:48 +0300

    Add a map to the about component

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,37 @@
-export function About(){
+import React from "react"
+import GoogleMapReact from "google-map-react"
+
+const AnyReactComponent = ({ loc }) => <div className="marker">{loc}</div>;
+
+export function About() {
+    const zoom = 9
+    const locations = [
+        { lat: 32.0853, lng: 34.7818 },
+        { lat: 32.0722, lng: 34.8265 },
+        { lat: 32.1658, lng: 34.8455 },
+        { lat: 32.4343, lng: 34.9196 },
+        { lat: 32.5735, lng: 34.9512 },
+    ]
+
     return (
-        <section>
-            <h2>About Me</h2>
-            <p>This site was made by Jonathan Menashe for Coding Academy</p>
+        <section className="about">
+            <h1>Our locations:</h1>
+            <div className="map" style={{ height: '65vh', width: '100%' }}>
+                <GoogleMapReact
+                    bootstrapURLKeys={{ key: "AIzaSyBmOoIOdOQXCXs0HDPk0OzBpCCRDWwE3qQ" }}
+                    center={{ lat: 32.1658, lng: 34.8455 }}
+                    defaultZoom={zoom}
+                >
+                    {locations.map(location => (
+                        <AnyReactComponent
+                            key={`${location.lat}-${location.lng}`}
+                            lat={location.lat}
+                            lng={location.lng}
+                            loc="📍"
+                        />
+                    ))}
+                </GoogleMapReact>
+            </div>
         </section>
     )
 }
