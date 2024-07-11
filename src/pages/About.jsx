bbb7c346674830464d15bd2a70e8e9ccc608b888
JoJobani <hvu2309@gmail.com>
2024-07-11 15:21:11 +0300

    Add map locations

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,29 +1,53 @@
-import React from "react"
+import { useState } from "react"
+import Button from '@mui/material/Button'
+import ButtonGroup from '@mui/material/ButtonGroup'
 import GoogleMapReact from "google-map-react"
 
-const AnyReactComponent = ({ loc }) => <div className="marker">{loc}</div>;
+const API_KEY = import.meta.env.GOOGLE_MAP_API || 'AIzaSyBmOoIOdOQXCXs0HDPk0OzBpCCRDWwE3qQ'
+const Marker = ({ loc }) => <div className="marker">{loc}</div>;
 
 export function About() {
-    const zoom = 9
+    const [coords, setCoords] = useState({ lat: 32.1658, lng: 34.8455 })
+    const [zoom, setZoom] = useState(9)
     const locations = [
-        { lat: 32.0853, lng: 34.7818 },
-        { lat: 32.0722, lng: 34.8265 },
-        { lat: 32.1658, lng: 34.8455 },
-        { lat: 32.4343, lng: 34.9196 },
-        { lat: 32.5735, lng: 34.9512 },
+        { city: 'Tel Aviv', lat: 32.0853, lng: 34.7818 },
+        { city: 'Ramat Gan', lat: 32.0722, lng: 34.8265 },
+        { city: 'Herzliya', lat: 32.1658, lng: 34.8455 },
+        { city: 'Hadera', lat: 32.4343, lng: 34.9196 },
+        { city: 'Zikhron Yaakov', lat: 32.5735, lng: 34.9512 },
     ]
 
     return (
         <section className="about">
             <h1>Our locations:</h1>
+            <div className="locations">
+                {locations.map(loc => {
+                    return (
+                        <Button
+                            key={loc.city}
+                            variant="outlined"
+                            onClick={() => {
+                                setCoords(loc)
+                                setZoom(12)
+                            }}
+                            sx={{
+                                margin: '5px'
+                            }}>
+                            {loc.city}
+                        </Button>
+                    )
+                })}
+            </div>
+
             <div className="map" style={{ height: '65vh', width: '100%' }}>
                 <GoogleMapReact
-                    bootstrapURLKeys={{ key: "AIzaSyBmOoIOdOQXCXs0HDPk0OzBpCCRDWwE3qQ" }}
-                    center={{ lat: 32.1658, lng: 34.8455 }}
-                    defaultZoom={zoom}
+                    bootstrapURLKeys={{ key: API_KEY }}
+                    center={coords}
+                    zoom={zoom}
+                    onClick={({ lat, lng }) => setCoords({ lat, lng })}
                 >
                     {locations.map(location => (
-                        <AnyReactComponent
+                        <Marker
                             key={`${location.lat}-${location.lng}`}
                             lat={location.lat}
                             lng={location.lng}
