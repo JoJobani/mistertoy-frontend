620385dfa0dde714c96593125b815264e4923bed
JoJobani <hvu2309@gmail.com>
2024-07-16 16:28:32 +0300

    Add chat typing detection

--- src/cmps/ChatRoom.jsx
+++ src/cmps/ChatRoom.jsx
@@ -1,18 +1,21 @@
 import React, { useState, useEffect } from 'react'
 import { useSelector } from 'react-redux'
 
-import { socketService, SOCKET_EMIT_SEND_MSG, SOCKET_EVENT_ADD_MSG, SOCKET_EMIT_SET_TOPIC } from '../services/socket.service.js'
+import { socketService, SOCKET_EMIT_SEND_MSG, SOCKET_EVENT_ADD_MSG, SOCKET_EMIT_SET_TOPIC, SOCKET_EMIT_USER_TYPING, SOCKET_EVENT_USER_TYPING } from '../services/socket.service.js'
 
 export function ChatRoom({ toy }) {
     const [msg, setMsg] = useState({ txt: '' })
     const [msgs, setMsgs] = useState([])
+    const [typingUsers, setTypingUsers] = useState([])
     const loggedInUser = useSelector(storeState => storeState.userModule.loggedinUser)
 
     useEffect(() => {
         socketService.on(SOCKET_EVENT_ADD_MSG, addMsg)
+        socketService.on(SOCKET_EVENT_USER_TYPING, handleUserTyping)
         socketService.emit(SOCKET_EMIT_SET_TOPIC, toy.name)
         return () => {
             socketService.off(SOCKET_EVENT_ADD_MSG, addMsg)
+            socketService.off(SOCKET_EVENT_USER_TYPING, handleUserTyping)
         }
     }, [toy])
 
@@ -26,11 +29,25 @@ export function ChatRoom({ toy }) {
         const newMsg = { from, txt: msg.txt }
         socketService.emit(SOCKET_EMIT_SEND_MSG, newMsg)
         setMsg({ txt: '' })
+        socketService.emit(SOCKET_EMIT_USER_TYPING, { user: from, isTyping: false })
+    }
+
+    function handleUserTyping({ user, isTyping }) {
+        setTypingUsers(prevUsers => {
+            if (isTyping && !prevUsers.includes(user)) {
+                return [...prevUsers, user]
+            } else if (!isTyping) {
+                return prevUsers.filter(u => u !== user)
+            }
+            return prevUsers
+        })
     }
 
     function handleFormChange(ev) {
         const { name, value } = ev.target
         setMsg(prevMsg => ({ ...prevMsg, [name]: value }))
+        socketService.emit(SOCKET_EMIT_USER_TYPING,
+            { user: loggedInUser?.fullname, isTyping: value.length > 0 })
     }
 
     return (
@@ -51,6 +68,10 @@ export function ChatRoom({ toy }) {
             <ul className='chat'>
                 {msgs.map((msg, idx) => (<li key={idx}>{msg.from}: {msg.txt}</li>))}
             </ul>
+
+            {typingUsers.length > 0 && (
+                <p>{typingUsers.join(', ')} {typingUsers.length === 1 ? 'is' : 'are'} typing...</p>
+            )}
         </section>
     )
 }
